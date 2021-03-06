2179478729e7c0d011c20bd7aa09af0c7f83c4ff
Mark Alexander <6917497+m15e@users.noreply.github.com>
2021-03-06 17:34:50 +0800

    UPDATE auth#spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
     let(:user) { FactoryBot.create(:user, username: 'react-frontend', password: 's3cr3t') }
+    let!(:token) { AuthenticationTokenService.call(user.id) } 
 
     it 'authenticates the client' do
       post '/api/v1/authenticate', params: { username: user.username, password: 's3cr3t' }
 
       expect(response).to have_http_status(:created)
-      # expect(response_body).to eq({
-      #   'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMX0.kjMQy4RNgidOsPRIwWce-30gxIpBXsbOYqOGZxjdhSE'
-      #  })
+      expect(response_body).to eq({
+        'favorites' => [],
+        'id' => user.id,
+        'token' => token,
+       })
     end
 
     it 'returns error when username is missing' do
