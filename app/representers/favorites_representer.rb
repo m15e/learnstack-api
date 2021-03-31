6bbba230a527fe2851b4b2f8bcd9d8eac3bf705b
Mark Alexander <6917497+m15e@users.noreply.github.com>
2021-03-31 22:20:59 +0800

    UPDATE favorites representer

--- app/representers/favorites_representer.rb
+++ app/representers/favorites_representer.rb
@@ -4,7 +4,14 @@ class FavoritesRepresenter
   end
 
   def as_json
-    favorites.map(&:stack_id)
+    #favorites.map(&:stack_id)
+    favorites.map do |favorite|
+      {
+        id: favorite.id,
+        user_id: favorite.user_id,
+        stack_id: favorite.stack_id,
+      }
+    end
   end
 
   private
